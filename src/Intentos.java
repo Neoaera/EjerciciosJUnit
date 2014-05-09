a921b899dec92f9e607d75d4cb91e2d90f51e7fd
Neoaera <aera_10@hotmail.com>
2014-05-08 20:57:28 -0400

    A la primera ventana no le entran balas. Por algun motivo valida letras en la segunda. ya no se cae!

--- src/Intentos.java
+++ src/Intentos.java
@@ -4,38 +4,40 @@ public class Intentos {
 
 	public static void main(String[] args) {
 		int njuegos = 2;
-		String marinwins, jonawins,ajustar;
-		String comprobar,asegurar="";
+		String marinwins, jonawins;
+		String comprobar, asegurar,nohackear = "";
 		int marintotal, jonatotal;
-		
 
 		while (njuegos != 0) {
-			njuegos = Integer.parseInt(JOptionPane.showInputDialog(null,
-					"Ingrese el numero de juegos"));
-			if (njuegos != 0) {
-				String resultados = JOptionPane.showInputDialog(null,
-						"Ingrese el resultado de los juegos");
-				ajustar = resultados.replace(" ", "");
-				comprobar= ajustar.replace("1", "");
-				asegurar= ajustar.replace("0","");
+			try{
+			njuegos = Integer.parseInt(JOptionPane.showInputDialog(null,"Ingrese el numero de juegos"));
+			if (njuegos > 0) {
+				String resultados = JOptionPane.showInputDialog(null,"Ingrese el resultado de los juegos");
+				String ajustar = resultados.replace(" ", "");
+				comprobar = ajustar.replace("1", "");
+				asegurar = ajustar.replace("0", "");
 				
 				if (njuegos == ajustar.length()) {
-					
-					marinwins = ajustar.replace("0", "");
-					jonawins = ajustar.replace("1", "");
-					marintotal = njuegos - marinwins.length();
-					jonatotal = njuegos - jonawins.length();
+					if (comprobar.length() != 0 || asegurar.length() != 0) {
+						marinwins = ajustar.replace("0", "");
+						jonawins = ajustar.replace("1", "");
+						marintotal = njuegos - marinwins.length();
+						jonatotal = njuegos - jonawins.length();
 
-					System.out.println("mary won " + marintotal
-							+ " times and, john won " + jonatotal + " times");
+						System.out.println("mary won " + marintotal
+								+ " times and, john won " + jonatotal
+								+ " times");
+					}
 				} else {
 					JOptionPane.showMessageDialog(null,"no ha ingresado la cantidad correcta de jugadas o ha ingresado un numero distinto a 0/1 HACES A MARIA LLORAR! D:<");
 				}
-			}else{
+			} else {
 				JOptionPane.showMessageDialog(null,"las veces que jugaron no puede ser menor que cero MARIA Y YO ESTAMOS MUY DECEPCIONADOS! D:<");
 				njuegos++;
 			}
-
+		}catch(Exception e){
+			JOptionPane.showMessageDialog(null,"eres terrible, hiciste alque que no debias");
+		}
 		}
 	}
 }
